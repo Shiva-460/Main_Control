2452fe217ae127a5869307bd1f74aa4d81e4ea15
Spencer-Sawyer <42276949+Spencer-Sawyer@users.noreply.github.com>
2019-02-24 00:14:45 -0800

    Robot test loop

--- robotcode/lineSensor.ino
+++ robotcode/lineSensor.ino
@@ -6,6 +6,10 @@ class lineSensor
  {
   
  }
+ uint32_t value()
+ {
+  
+ }
   boolean isBright()
   {
     //Calibrate for a white line, If it's longer a predetermined setting then we cut and return false
@@ -16,4 +20,4 @@ class lineSensor
     return !isBright();
   }
   
-}
+};
