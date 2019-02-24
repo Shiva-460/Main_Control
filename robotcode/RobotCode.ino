9c6cc40d780933e995fe238c3c510f6ec5c59472
spencer-sawyer <42276949+Spencer-Sawyer@users.noreply.github.com>
2019-02-24 01:27:58 -0800

    sensor libraries finished for the most part

--- robotcode/RobotCode.ino
+++ robotcode/RobotCode.ino
@@ -1,4 +1,5 @@
 #include "disSensor.h"
+#include "lineSensor.h"
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -6,7 +7,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-
+/*
 distanceSensor backLeft(2),frontLeft(3),backRight(4),frontRight(5);
 
 
@@ -15,6 +16,10 @@ distanceSensor backLeft(2),frontLeft(3),backRight(4),frontRight(5);
 Serial.print("\nBack left:");Serial.print(backLeft.distance());
 Serial.print("\nFront left:");Serial.print(frontLeft.distance());
 Serial.print("\nBack right:");Serial.print(backRight.distance());
-Serial.print("\nFront right:");Serial.print(frontRight.distance());
+Serial.print("\nFront right:");Serial.print(frontRight.distance());*/
+
+lineSensor left(45),right(44);
+Serial.print("\nleft bright?:");Serial.print(left.isBright());
+Serial.print("\nright bright?:");Serial.print(right.isBright());
 delay(1000);
 }
